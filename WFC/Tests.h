a2cf87edf472463da80aef0005a7d01710d27b7b
Ted Kahl <tedkahl@gmail.com>
2020-09-04 16:04:03 -0700

    Change tests to remove pattern, add propagator

--- WFC/Tests.h
+++ WFC/Tests.h
@@ -1,23 +1,23 @@
 #pragma once
 #include "Pattern.h"
-
+#include "Propagator.h"
+#include "WFCOptions.h"
 
 template<typename T>
-void printPattern(Pattern<T> &p) {
-	for (auto i : p.matrix_) {
+void printPattern(matrix<T> &p) {
+	for (auto i : p) {
 		for (auto j : i) std::cout << j;
 		std::cout << std::endl;
 	}
 }
 
 template <typename T>
-void printOverlaps(const Pattern<T> &p)
+void printOverlaps(const size_t index, const Propagator<T> &prop, const WFCOptions options)
 {
-	int n = p.matrix_.size();
-	for (int i = 0; i < p.rules.size(); i++) {
-		for (int j = 0; j < p.rules[0].size(); j++) {
-			std::cout << i - n << ", " << j - n << ":" << std::endl;
-			for (auto k : p.rules[i][j]) {
+	for (int i = 0; i < 2*options.n-1; i++) {
+		for (int j = 0; j < 2*options.n-1; j++) {
+			std::cout << i - options.n << ", " << j - options.n << ":" << std::endl;
+			for (auto k : prop.rules[index][i][j]) {
 				printPattern(*k);
 				std::cout << std::endl;
 			}
