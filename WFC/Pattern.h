5ad2fe532d339fa6e0579fc27497530307c60761
Ted Kahl <tedkahl@gmail.com>
2020-09-04 16:01:47 -0700

    remove pattern class

--- WFC/Pattern.h
+++ WFC/Pattern.h
@@ -6,36 +6,6 @@
 template<class T>
 using matrix = std::vector<std::vector<T>>;
 
-template <typename T>
-struct Pattern
-{
-public:
-	matrix<T> matrix_;
-	unsigned weight=1; //frequency of pattern in input image
-
-	bool allowed(const Pattern<T> &pattern, int yoffset, int xoffset);
-
-	Pattern();
-	Pattern(std::initializer_list<std::initializer_list<T>> &&in);
-	Pattern(const matrix<T> &&other);
-	Pattern(const matrix<T> &other);
-
-
-	matrix<std::vector<std::shared_ptr<Pattern<T> > > > rules;
-
-	bool operator== (const Pattern& other);
-private: //stores "overlap rules"
-
-};
-
-template <typename T>
-bool Pattern<T>::operator==(const Pattern& other) {
-	return matrix_ == other.matrix_;
-}
-
-template <typename T>
-Pattern<T>::Pattern() {}
-
 namespace std {
 	template <typename T>
 	class hash<matrix<T>> {
@@ -51,66 +21,10 @@ namespace std {
 	};
 }
 
-
-/*template <typename T>
-Pattern<T>& Pattern<T>::operator=(const matrix<T> &&other) {
-	matrix = other;
-	other = nullptr;
-	return *this;
-}*/
-
-template <typename T>
-Pattern<T>::Pattern(std::initializer_list<std::initializer_list<T>> &&in)
-{
-	for (auto i : in) matrix_.push_back(i);
-}
-
-template <typename T>
-Pattern<T>::Pattern(const matrix<T> &&other)
-{
-	matrix_ = other;
-}
-
-template <typename T>
-Pattern<T>::Pattern(const matrix<T> &other)
-{
-	matrix_ = other;
-}
-
-/*Checks if pattern is allowed in given position according to current rules*/
-template <typename T>
-bool Pattern<T>::allowed(const Pattern<T> &pattern, int yoffset, int xoffset) {
-	for (auto i : allowed[yoffset - 1 + pattern.size()][xoffset - 1 + pattern.size()])
-		if (*i.matrix_ == pattern.matrix_) return true;
-	return false;
-}
-
-/*Intialize rules matrix for each detected pattern. Rules stores, for each possible overlap position, a pointer to
-each pattern that can overlap in that position. Other patterns are not permitted in that position.*/
-template <typename T>
-void setOverlapRules(std::vector<Pattern<T>> &patterns)
-{
-	size_t n = patterns[0].matrix_.size();
-	int rule_matrix_size = (n * 2 - 1);
-	for (auto i : patterns) {
-		for (int j = 0; j < rule_matrix_size; j++) {
-			i.rules.push_back({});
-			for (int k = 0; k < rule_matrix_size; k++) {
-				i.rules[j].push_back({});
-				for (auto l : patterns) {
-					if (overlaps(i, l, j + 1 - n, k + 1 - n))
-						i.rules[j][k].push_back(std::make_shared<Pattern<T>>(&l));
-				}
-			}
-		}
-	}
-}
-
-
 /*Determines if two patterns can overlap in a given configuration. yoffset and xoffset specify the position of b 
 relative to a*/
 template <typename T>
-bool overlaps(const Pattern<T> &a, const Pattern<T> &b, int yoffset, int xoffset)
+bool overlaps(const matrix<T> &a, const matrix<T> &b, const int yoffset, const int xoffset)
 {
 	size_t n = a.size();
 	size_t overlap_height = n - std::abs(yoffset);
@@ -118,8 +32,8 @@ bool overlaps(const Pattern<T> &a, const Pattern<T> &b, int yoffset, int xoffset
 	assert(std::abs(xoffset) < n);
 	assert(std::abs(yoffset) < n);
 
-	return (subMatrix(a.matrix_, std::max(0, yoffset), std::max(0, xoffset), overlap_height, overlap_width) ==
-		subMatrix(b.matrix_, std::max(0, -yoffset), std::max(0, -xoffset), overlap_height, overlap_width));
+	return (subMatrix(a, std::max(0, yoffset), std::max(0, xoffset), overlap_height, overlap_width) ==
+		subMatrix(b, std::max(0, -yoffset), std::max(0, -xoffset), overlap_height, overlap_width));
 }
 
 /*Returns submatrix of specified height and width, beginning at given offsets*/
@@ -136,27 +50,28 @@ matrix<T> subMatrix(const matrix<T> &mat, size_t yoffset, size_t xoffset, size_t
 
 /*Returns pattern reflected about the x axis*/
 template <typename T>
-Pattern<T> reflect(Pattern<T> &pattern) {
-	size_t n = pattern.matrix_.size();
+matrix<T> reflect(matrix<T> &pattern) {
+	size_t n = pattern.size();
 	matrix<T> reflected(n, std::vector<T>(n));
 	for (size_t i = 0; i < n; i++)
 		for (size_t j = 0; j < n; j++) {
-			reflected[i][n - 1 - j] = pattern.matrix_[i][j];
+			reflected[i][n - 1 - j] = pattern[i][j];
 		}
-	return Pattern<T>(reflected);
+	return reflected;
 
 }
 
 /*Returns the matrix rotated 90 degrees clockwise*/
 template <typename T>
-Pattern<T> rotate90(Pattern<T> &pattern)
+matrix<T> rotate90(matrix<T> &pattern)
 {
-	matrix<T> rotated(pattern.matrix_[0].size(),std::vector<T>(pattern.matrix_.size()));
-	for (size_t i = 0; i < pattern.matrix_.size(); i++)
-		for (size_t j = 0; j < pattern.matrix_[0].size(); j++) {
-			rotated[j][rotated[0].size()-1-i]=pattern.matrix_[i][j];
+	size_t n = pattern.size();
+	matrix<T> rotated(n, std::vector<T>(n));
+	for (size_t i = 0; i < n; i++)
+		for (size_t j = 0; j < n; j++) {
+			rotated[j][n - 1 - i]=pattern[i][j];
 		}
-	return Pattern<T>(rotated);
+	return rotated;
 }
 
 /*
--- WFC/WFC.h
+++ WFC/WFC.h
@@ -13,13 +13,14 @@ class WFC
 private:
 	matrix<T> input;
 	matrix<T> output;
-	std::vector<Pattern<T>> patterns;
+	std::vector<matrix<T>> patterns;
+	std::vector<unsigned> pattern_weights;
 	std::unordered_map<matrix<T>, size_t> pmap;
 	//Wave<T> wave;
 	WFCOptions options;
 public:
 	void getPatterns();
-	void insertRotations(Pattern<T> &pattern);
+	void insertRotations(matrix<T> &pattern);
 	void initOutput();
 	WFC(matrix<T> &input_, size_t oheight, size_t owidth, const unsigned n=default_n, bool rotate=true, bool reflect=true);
 };
@@ -31,9 +32,13 @@ WFC<T>::WFC(matrix<T> &input_, size_t oheight, size_t owidth, unsigned n, bool r
 	//input=vector<vector<T>>(owidth,vector<T>(oheight,T()));
 
 	getPatterns();
+	for (size_t i = 0; i < patterns.size();i++) {
+		printPattern(patterns[i]);
+		std::cout << "Weight: "<< pattern_weights[i]<<std::endl<<std::endl;
 
-	setOverlapRules(patterns);
-	for (auto i : patterns) printOverlaps(i);
+	}
+	//setOverlapRules(patterns);
+	//for (auto i : patterns) printOverlaps(i);
 }
 
 /*template <class T>
@@ -46,10 +51,10 @@ void WFC<T>::getInput(std::string fname) {
 
 template <class T>
 void WFC<T>::getPatterns() {
-	Pattern<T> pattern;
+	matrix<T> pattern;
 	for (size_t i = 0; i <= input.size() - options.n; i++) {
 		for (size_t j = 0; j <= input[0].size() - options.n;j++) {
-			pattern.matrix_ = subMatrix(input, i, j, options.n, options.n);
+			pattern = subMatrix(input, i, j, options.n, options.n);
 			insertRotations(pattern);
 			if (options.reflect) {
 				pattern = reflect(pattern);
@@ -60,16 +65,17 @@ void WFC<T>::getPatterns() {
 }
 
 template <class T>
-void WFC<T>::insertRotations(Pattern<T> &pattern) {
+void WFC<T>::insertRotations(matrix<T> &pattern) {
 	int rotations = options.rotate ? 4 : 1;
 	for (int i = 0; i < rotations; i++)
 	{
-		auto res = pmap.insert(std::make_pair(pattern.matrix_, patterns.size()));
+		auto res = pmap.insert(std::make_pair(pattern, patterns.size()));
 		if (res.second) {
 			patterns.push_back(pattern);
+			pattern_weights.push_back(1);
 		}
 		else {
-			patterns[res.first->second].weight++;
+			pattern_weights[res.first->second]++;
 		}
 		pattern = rotate90(pattern);
 	}
@@ -77,6 +83,6 @@ void WFC<T>::insertRotations(Pattern<T> &pattern) {
 
 template <class T>
 void WFC<T>::initOutput() {
-	output(std::vector<int>(options.owidth), options.oheight);
-	for (auto i : output) i.push_back(std::make_shared<std::vector < std::vector<T>>>(&i));
+	output(std::vector<T>(options.owidth), options.oheight);
+	for (auto i : output) i.push_back(std::make_shared<matrix<T>>(&i));
 }
