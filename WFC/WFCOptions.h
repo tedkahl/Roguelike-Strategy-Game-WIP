1bd457dc6c695a7b7967e2f2f1775454fd24fc28
Ted Kahl <tedkahl@gmail.com>
2020-09-06 21:26:03 -0700

    add operator=

--- WFC/WFCOptions.h
+++ WFC/WFCOptions.h
@@ -2,14 +2,26 @@
 #define WFCOPTIONS
 struct WFCOptions
 {
-	unsigned n;
+	size_t n;
 	size_t oheight;
 	size_t owidth;
 	bool rotate;
 	bool reflect;
 
 	WFCOptions(const size_t oheight_, const size_t owidth_, const unsigned n_, const bool rotate_, const bool reflect_);
+	WFCOptions& operator=(const WFCOptions &other);
+	WFCOptions() {};
 };
+
+WFCOptions& WFCOptions::operator=(const WFCOptions &other)
+{
+	n = other.n;
+	oheight = other.oheight;
+	owidth = other.owidth;
+	rotate = other.rotate;
+	reflect = other.reflect;
+	return *this;
+}
 
 WFCOptions::WFCOptions(size_t oheight_, size_t owidth_, unsigned n_, bool rotate_, bool reflect_)
 	:n(n_),oheight(oheight_),owidth(owidth_),rotate(rotate_),reflect(reflect_){}
