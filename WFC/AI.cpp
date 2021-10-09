88660af5322c793b412f218b27cb3143cd1ff37f
Ted Kahl <tedkahl@gmail.com>
2021-10-08 22:28:05 -0700

    two target from map functions, working basic AI

--- WFC/AI.cpp
+++ WFC/AI.cpp
@@ -1,5 +1,5 @@
 #include "AI.h"
-AIPlayer::AIPlayer(Level& level, int team) :Player(team), level_(level) {
+AIPlayer::AIPlayer(Level& level, int team) :PlayerState(team), level_(level), dj_map(nullptr), has_selection(false) {
 
 }
 bool AIPlayer::selectNext() {
@@ -7,25 +7,43 @@ bool AIPlayer::selectNext() {
 	if (u) {
 		//CALL DECISION PROCESS HERE
 		//REFER TO UNIT SPECIFIC INFO
-		currCommand = std::make_unique<AttackMove>(u->getOwner(), level_);
-		currCommand->showTargeter();
+		selected = u->getOwner();
+		command.reset(new AttackMove(selected, level_));
+		command->showTargeter();
 		has_selection = true;
 		return true;
 	}
 	return false;
 }
 
+void AIPlayer::rushClosest(Entity* me, sf::Time now) {
+	if (!command->execute(target_from_map(me, *dj_map), now, this)) {
+		unit_wait(me->uc());
+	}
+}
+
 void AIPlayer::executeSelected(sf::Time now) {
 	//target probably calculated in first step if we do this correctly
-	rushClosest(getNext()->getOwner(), level_, now, currCommand.get(), *dj_map);
+	rushClosest(selected, now);
+	setMoveState(selected->uc(), unit::move_state::HAS_ACTED);
 	has_selection = false;
 }
+
 //initializing dj map this way not really satisfactory but should work for now
 void AIPlayer::getMap(move_type type) {
 	dj_map = &djikstraMap(type, team_, enemies, level_.state);
+	for (unsigned i = 0;i < dj_map->height();i++) {
+		for (unsigned j = 0;j < dj_map->width();j++) {
+			cout << dj_map->at(j, i).moves_left;
+			if (dj_map->at(j, i).moves_left < 10) cout << '_';
+			cout << " ";
+
+		}
+		cout << endl;
+	}
 }
 
 void AIPlayer::startTurn() {
-	getMap(move_type::WALK);
 	getUnits(level_.units);
+	getMap(move_type::WALK);
 }
--- WFC/AI.h
+++ WFC/AI.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <algorithm>
-#include "Player.h"
+#include "PlayerState.h"
 #include "UnitComponent.h"
 #include "DataManager.h"
 #include "Team.h"
@@ -13,28 +13,63 @@
 //	std::sort(enemies.begin(), enemies.end(), entityCmp);
 //}
 
-
 //this is actually pretty insufficient when there are changes mid-turn
-static sf::Vector2i target_from_map(Entity* me, matrix<map_node>& dj_map) {
+static sf::Vector2i target_from_map(Entity* me, matrix<dj_map_node>& dj_map) {
+	auto pos = me->getPos();
+
+	int start_score = dj_map.at(pos).moves_left;
+	auto next_loc = dj_map.at(pos).prev;
+	auto next_next = dj_map.at(next_loc).prev;
+	while (next_next.x != -1 && start_score - dj_map.at(next_next).moves_left <= me->uc()->stats().movement) {
+		cout << start_score - dj_map.at(next_next).moves_left << endl;
+		cout << me->uc()->stats().movement << endl;
+		next_loc = next_next;
+		next_next = dj_map.at(next_next).prev;
+	}
+	return next_loc;
+}
+
+//select the best squares
+//can return a list of possible targets and can even be used to construct targeter in theory
+static std::vector<sf::Vector2i> target_from_map_2(Entity* me, matrix<dj_map_node>& dj_map, Board& board) {
 	auto pos = me->getPos();
 	int start_score = dj_map.at(pos).moves_left;
-	auto& next_loc = dj_map.at(dj_map.at(pos).prev);
-	while (next_loc.moves_left != 0 && start_score - next_loc.moves_left >= me->uc()->stats().movement) {
-		next_loc = dj_map.at(next_loc.prev);
+	int width = 1;
+	sf::Vector2i d;
+	sf::Vector2i next_dir;
+	sf::Vector2i sq;
+	int min_score = INT_MAX;
+	std::vector<sf::Vector2i> targets(1, pos);
+	while (width <= me->uc()->stats().movement) {
+		for (int i = 0;i < 4;i++) {
+			d = dir[i];
+			next_dir = dir[(i + 1) % 4];
+			sq = pos + d * width;
+			while (sq != pos + next_dir * width) {
+				sq -= (dir[i] - next_dir);
+				if (start_score - dj_map.at(sq).moves_left >= me->uc()->stats().movement && !board.board.at(sq).unit()) {
+					if (dj_map.at(sq).moves_left < min_score) {
+						targets = { sq };
+						min_score = dj_map.at(sq).moves_left;
+					}
+					else if (dj_map.at(sq).moves_left == min_score) {
+						targets.push_back(sq);
+					}
+				}
+			}
+		}
+		++width;
 	}
-	return next_loc.prev;
+	return targets;
 }
 
-static void rushClosest(Entity* me, Level& level, sf::Time now, Command* cmd, matrix<map_node>& dj_map) {
-	cmd->execute(target_from_map(me, dj_map), now);
-}
 
-class AIPlayer :public Player {
+class AIPlayer :public PlayerState {
 private:
-	std::unique_ptr<Command> currCommand;
 	bool has_selection;
 	Level& level_;
-	matrix<map_node>* dj_map;
+	matrix<dj_map_node>* dj_map;
+	void rushClosest(Entity* me, sf::Time now);
 	//virtual void getNextMove();
 	void getMap(move_type type);
 public:
