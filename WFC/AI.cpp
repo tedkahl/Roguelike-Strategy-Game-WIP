1155089ec9cab50708aa9d4c155c8d40b862688b
Ted Kahl <tedkahl@gmail.com>
2021-10-12 10:29:00 -0700

    AI closer to final decision making process, but not done

--- WFC/AI.cpp
+++ WFC/AI.cpp
@@ -1,37 +1,81 @@
 #include "AI.h"
-AIPlayer::AIPlayer(Level& level, int team) :PlayerState(team), level_(level), dj_map(nullptr), has_selection(false) {
+#include "Attacks.h"
+
+//bool operator<(const sf::Vector2i& l, const sf::Vector2i& r) { return std::tie(l.x, l.y) < std::tie(r.x, r.y); }
+auto actualTargets(matrix<dj_map_node>& dj_map, std::vector<sf::Vector2i>& move_targets) {
+	auto cmp = [](const sf::Vector2i& lhs, const sf::Vector2i& rhs) {return compV2i(lhs, rhs);};
+	std::set<sf::Vector2i, decltype(cmp)> ret;
+	for (auto& i : move_targets) {
+		for (auto j : dj_map.at(i).reachable_targets) {
+			ret.insert(j);
+		}
+	}
+	return ret;
+}
+void AIPlayer::decideFinalTarget(Entity* e, matrix<dj_map_node>& dj_map, std::vector<sf::Vector2i>& move_targets) {
+	auto t = actualTargets(dj_map, move_targets);
+	command.reset(new AttackMove(e, level_));
+	target = *t.begin();
+}
+
+//select the best move locations from djikstra map
+static std::vector<sf::Vector2i> moveTargetsFromMap(Entity* me, matrix<dj_map_node>& dj_map, pathsGrid& paths, Board& board) {
+	auto pos = me->getPos();
+	int min_score = INT_MAX;
+	std::vector<sf::Vector2i> move_targets(1, pos);
+	sf::Vector2i sq;
+	for (int i = 0;i < paths.grid.width();i++) {
+		for (int j = 0;j < paths.grid.height();j++) {
+			if (paths.grid.at(i, j).search == paths.search) {
+				sq = sf::Vector2i{ i,j } + paths.offset;
+
+				if (paths.grid.at(i, j).movable && !paths.grid.at(i, j).has_ally) {
+					if (dj_map.at(sq).moves_left < min_score) {
+						move_targets = { sq };
+						min_score = dj_map.at(sq).moves_left;
+					}
+					else if (dj_map.at(sq).moves_left == min_score) {
+						move_targets.push_back(sq);
+					}
+				}
+			}
+		}
+	}
+	return move_targets;
+}
+
+AIPlayer::AIPlayer(Level& level, int team) :PlayerState(team), level_(level), dj_map(nullptr) {
 
 }
+
+// select unit
+// get paths
+// choose target and command
+// show targeter
+// return
 bool AIPlayer::selectNext() {
 	auto u = getNext();
 	if (u) {
-		//CALL DECISION PROCESS HERE
-		//REFER TO UNIT SPECIFIC INFO
 		selected = u->getOwner();
-		command.reset(new AttackMove(selected, level_));
+		auto paths = pathFind(selected->uc(), level_.state, getAttack<map_node>(u->stats().attack_type));
+		std::vector<sf::Vector2i> targets = moveTargetsFromMap(selected, *dj_map, paths, level_.state);
+		decideFinalTarget(selected, *dj_map, targets);
 		command->showTargeter();
-		has_selection = true;
 		return true;
 	}
 	return false;
 }
 
-void AIPlayer::rushClosest(Entity* me, sf::Time now) {
-	if (!command->execute(target_from_map(me, *dj_map), now, this)) {
-		unit_wait(me->uc());
-	}
-}
 
 void AIPlayer::executeSelected(sf::Time now) {
-	//target probably calculated in first step if we do this correctly
-	rushClosest(selected, now);
-	setMoveState(selected->uc(), unit::move_state::HAS_ACTED);
-	has_selection = false;
+	command->execute(target, now, this);
+	deSelect();
+	deleteCommand();
 }
 
 //initializing dj map this way not really satisfactory but should work for now
 void AIPlayer::getMap(move_type type) {
-	dj_map = &djikstraMap(type, team_, enemies, level_.state);
+	dj_map = &djikstraMap(type, team_, enemies, level_.state, getAttack<dj_map_node>(attack_type::MELEE));
 	for (unsigned i = 0;i < dj_map->height();i++) {
 		for (unsigned j = 0;j < dj_map->width();j++) {
 			cout << dj_map->at(j, i).moves_left;
@@ -44,6 +88,6 @@ void AIPlayer::getMap(move_type type) {
 }
 
 void AIPlayer::startTurn() {
-	getUnits(level_.units);
+	p_start_turn(level_.units);
 	getMap(move_type::WALK);
 }
