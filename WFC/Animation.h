104752f81de07ff5841021ae4757a71b85e4b282
Ted Kahl <tedkahl@gmail.com>
2021-10-16 22:54:50 -0700

    also now that animations can be updated by RealTime, greatly simplify Animation class

--- WFC/Animation.h
+++ WFC/Animation.h
@@ -8,89 +8,40 @@
 using std::cout;
 using std::endl;
 
-const int max_seg = 3;
-//start at a given rect, and move right (mod width) for a certain number of frames
-struct AnimationSeg {
+const int max_seg = 1;
+
+//start at a given rect, and move right along the sprite sheet (mod width) for a certain number of frames
+//More complex animations, like changing directions or jumping during movement, handled by RealTime piecing these together. 
+struct Animation {
 	sf::IntRect starting;
-	unsigned short loop_length; //ms for now
+	uint16_t start_time;
+	uint16_t loop_length; //ms for now
+	uint16_t pause;
+	bool loop;
 	uint8_t frames;
-	uint8_t loops;
 	inline float frame_duration() const { return static_cast<float>(loop_length) / frames; }
-	inline unsigned short length() const { return loop_length * loops; }
+
+	sf::IntRect getRect(sf::Time current, int sheet_width) {
+		//get our current frame
+		uint16_t position = static_cast<int>((current.asMilliseconds() % (loop_length + pause) - start_time) / frame_duration());
+		//if frame is greater than max frames, deactivate if loop is not set. Else pause.
+		if (position >= frames) {
+			if (!loop)
+				frames = 0;
+			return { -1,-1,-1,-1 };
+		}
+		int x = (starting.left + position * starting.width);
+		return sf::IntRect(x % sheet_width, (starting.top + starting.height * (x / sheet_width)), starting.width, starting.height);
+	}
+	bool active() { return frames != 0; }
 	void print() {
 		cout << to_str(starting);
-		cout << "ll: " << loop_length << " frames: " << static_cast<int>(frames) << " loops: " << static_cast<int>(loops) << endl << endl;
+		//cout << "ll: " << static_cast<int>(loop_length) << " frames: " << static_cast<int>(frames) << " pause: " << static_cast<int>(pause) << endl;
 	}
-	AnimationSeg() = default;
-	AnimationSeg(const sf::IntRect& start, unsigned short loop_len, uint8_t num_frames, uint8_t num_loops) :starting(start), loop_length(loop_len), frames(num_frames), loops(num_loops) {}
-};
-
-//store a series of animation segments and get texture rectangle from them. 
-template<size_t SEG_MAX>
-struct Animation {
-private:
-	sf::Time start_time;
-
-	float ms(sf::Time current) const { return static_cast<int>(((current - start_time).asMilliseconds() - time_mod) * speed_mult); }
-	uint8_t segments;
-	float speed_mult;
-	bool loop_;
-	uint8_t index;
-	unsigned short time_mod; //time taken by previous segments
-	std::array<AnimationSeg, SEG_MAX> animation;
-public:
-	void set(sf::Time start, std::vector<AnimationSeg>&& segs, bool loop = false, float speed = 1.f) {
-		start_time = start;
-		segments = segs.size();
-		for (unsigned i = 0;i < segs.size();i++) {
-			animation[i] = segs[i];
-		}
-		loop_ = loop;
-		speed_mult = speed;
-		time_mod = 0;
-		index = 0;
-		/*cout << "initiating animation" << endl;
-		int i = 0;
-		for (int i = 0;i < segments;i++) {
-			animation[i].print();
-		}*/
-	}
-
-	bool active() { return segments != 0; }
-	inline void clear()
-	{
-		segments = 0;loop_ = false;
-	}
-	sf::IntRect getRect(sf::Time current, int sheet_width) {
-		int position = static_cast<int>((ms(current) / animation[index].frame_duration()));
-		//cout << "position " << position << endl;
-		//cout << "frames " << (int)animation[index].frames << endl;
-		//cout << "loops " << (int)animation[index].loops << endl;
-		//cout << "multiplied " << (int)animation[index].frames * animation[index].loops << endl;
-		if (position >= (int)animation[index].frames * animation[index].loops) {
-			//cout << "finished loop" << endl;
-			position = 0;
-			time_mod = animation[index].length();
-			index++;
-		}
-		if (index == segments) {
-			//cout << "index==segments" << endl;
-			if (loop_) {
-				index = 0;
-				time_mod = 0;
-			}
-			else {
-				clear();
-				return sf::IntRect(-1, -1, -1, -1);
-			}
-		}
-		position %= animation[index].frames;
-		int x = (animation[index].starting.left + animation[index].starting.width * position);
-		//auto rect = 
-		//cout << "updating rect: " << rect.left << ", " << rect.top << ", " << rect.width << ", " << rect.height << endl;
-		return sf::IntRect(x % sheet_width, (animation[index].starting.top + animation[index].starting.height * (x / sheet_width)), animation[index].starting.width, animation[index].starting.height);
-
-	}
+	void start(sf::Time time) { start_time = time.asMilliseconds() % (loop_length + pause); }
+	Animation() = default;
+	inline void setLooping(bool looping) { loop = looping; }
+	Animation(const sf::IntRect& start, unsigned short loop_len, uint8_t num_frames, uint8_t num_loops = 1, unsigned short pause_after_loop = 0) :starting(start), start_time(0), loop_length(loop_len), frames(num_frames), pause(pause_after_loop), loop(false) {}
 };
 
 
--- WFC/AnimationManager.h
+++ WFC/AnimationManager.h
@@ -5,17 +5,19 @@ constexpr int num_units = static_cast<int>(object_type::OBJECT_END) - firstunit;
 const int sprite_dim = 72;
 constexpr int x = unitIndex(object_type::DUELIST);
 
-const int S = 3;
+
 struct simple_unit_anim
 {
 private:
 	struct anim_info
 	{
+		//row that animation starts at (for now must start at the beginning of a row)
 		uint8_t row;
+		//frames in animation
 		uint8_t frames;
 	};
 	bool is_set = false;
-	std::array<anim_info, static_cast<int>(anim_state::ANIM_STATE_END)> anim_rows_frames;
+	std::array<anim_info, static_cast<int>(anim_state::ANIM_STATE_END)> anim_rows_frames = {};
 public:
 	void set(std::array<anim_info, static_cast<int>(anim_state::ANIM_STATE_END)> data)
 	{
@@ -27,6 +29,7 @@ public:
 	}
 };
 
+//this is not how it's going to work. 
 class animation_manager {
 private:
 	animation_manager() {
@@ -47,17 +50,13 @@ public:
 		return std::max(150 * info.frames, 500);
 	}
 
-	//std::vector<AnimationSeg> get_anim()
-	std::vector<AnimationSeg> get_pause_anim(sf::IntRect rect, int duration) {
-		return std::vector<AnimationSeg>(1, AnimationSeg(rect, duration, 1, 1));
-	}
-	std::vector<AnimationSeg> get_basic_anim(object_type otype, anim_state state, uint8_t loops, unsigned short loop_length = 0) {
+
+	Animation get_basic_anim(object_type otype, anim_state state, unsigned short loop_length = 0) {
 		auto& info = anims[unitIndex(otype)].get(state);
 		cout << "row: " << (int)info.row << " frames " << (int)info.frames << endl;
 		if (loop_length == 0)
 			loop_length = get_loop_length(otype, state);
-		AnimationSeg myseg(sf::IntRect(0, sprite_dim * info.row, sprite_dim, sprite_dim), loop_length, info.frames, loops);
-		cout << to_str(myseg.starting) << ", " << myseg.loop_length << ", " << (int)myseg.frames << ", " << (int)myseg.loops << endl;
-		return std::vector<AnimationSeg>(1, std::move(myseg));
+
+		return Animation(sf::IntRect(0, sprite_dim * info.row, sprite_dim, sprite_dim), loop_length, info.frames);
 	}
 };
