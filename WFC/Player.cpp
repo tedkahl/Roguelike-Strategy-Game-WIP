af6934ed65ec11e5fe77e3237186f2aa1814f3a4
Ted Kahl <tedkahl@gmail.com>
2021-11-11 11:27:25 -0800

    revamp player class structure to be a bit more sensible, + use ranges instead of vectors of pointers

--- WFC/Player.cpp
+++ WFC/Player.cpp
@@ -1,48 +1,71 @@
+#include <ranges>
 #include "Player.h"
-#include <iostream>
+#include "Command.h"
+#include "UnitManager.h"
+#include "Level.h"
+
+Player::Player(Level& level, int team) :team_(team), turn(0), level_(level), selected(nullptr) {}
+void Player::switchCommand(Command* new_command) {
+	command.reset(new_command);
+}
+
+void Player::deleteCommand() {
+	command = nullptr;
+}
+
+void Player::deSelect() {
+	selected = nullptr;
+}
+
+void Player::unit_wait(UnitComponent* u) {
+	setMoveState(selected->uc(), unit::move_state::HAS_ACTED);
+	deSelect();
+	deleteCommand();
+}
+
+void Player::startTurn() {
+	turn++;
+	done_ = false;
+	for (auto& i : (*level_.units) | std::views::filter(unit::allies(team_))) {
+		i.movestate = unit::move_state::FREE;
+	}
+}
+
+//get the next movable unit by searching through the list from scratch. This is fine because it's a small contiguous list and this ensures any changes to the unit list are reflected in selection
+UnitComponent* Player::getNext() {
+	using namespace std::views;
+	//get range of movable allies
+	auto movable = (*level_.units) | filter(unit::allies(team_)) | filter(unit::movable());
+
+	//if range is empty return null
+	auto start = movable.begin();
+	if (start == movable.end()) return nullptr;
+
+	//else get the next unit
+	else if (selected) {
+		while (&*start <= selected->uc()) start++;
+
+		if (start == movable.end()) {
+			return &*movable.begin();
+		}
+	}
+	return &*start;
+}
+unsigned Player::count_movable() {
+	unsigned count = 0;
+	using namespace std::views;
+	for (auto const& i : (*level_.units) | filter(unit::allies(team_)) | filter(unit::movable())) count++;
+	cout << "movable units: " << count << endl;
+	return count;
+}
 void Player::setMoveState(UnitComponent* u, std::optional<unit::move_state> new_state) {
+	using namespace std::views;
 	if (!new_state) return;
 	if (u->movestate != unit::move_state::HAS_ACTED && new_state.value() == unit::move_state::HAS_ACTED)
 		num_moved++;
 	else if (u->movestate == unit::move_state::HAS_ACTED && new_state.value() != unit::move_state::HAS_ACTED)
 		num_moved--;
 	u->movestate = new_state.value();
-}
+	if (count_movable() == 0) done_ = true;
 
-//make this dumb for now why bother bookkeeping
-//or not
-UnitComponent* Player::getNext(DataManager<UnitComponent>& units) {
-	if (units.isChanged())
-		getUnits(units);
-	if (num_moved >= my_team.size()) {
-		std::cout << "num moved = team size, turn over" << std::endl;
-		return nullptr;
-	}
-	return my_team[num_moved];
-}
-
-void Player::p_start_turn(DataManager<UnitComponent>& units) {
-	num_moved = 0;
-	units.setChanged(true);
-	getUnits(units);
-	//warning, may not always hold
-	for (auto i : my_team) {
-		i->movestate = unit::move_state::FREE;
-	}
-}
-
-void Player::getUnits(DataManager<UnitComponent>& units) {
-	assert(units.isChanged());
-	my_team.clear();
-	enemies.clear();
-	for (auto& i : units) {
-		auto en = getEnmity(team_, &i);
-		if (en == enmity_t::ENEMY) {
-			enemies.push_back(&i);
-		}
-		else if (en == enmity_t::SAME_TEAM) {
-			my_team.push_back(&i);
-		}
-	}
-	units.setChanged(false);
 }
--- WFC/Player.h
+++ WFC/Player.h
@@ -1,26 +1,34 @@
 #pragma once
-#include <vector>
-#include <optional>
 #include "UnitComponent.h"
-#include "Team.h"
-#include "DataManager.h"
+#include <memory>
+#include <optional>
+class Command;
+class Level;
+class UnitManager;
+class Entity;
 class Player {
 protected:
 	int team_;
-	int num_moved;
-	std::vector<UnitComponent*> my_team;
-	std::vector<UnitComponent*> enemies;
-	void p_start_turn(DataManager<UnitComponent>& units);
+	int turn;
+	Level& level_;
+	bool done_;
+	int num_moved = 0;
+	unsigned count_movable();
 public:
-	Player(int team) : team_(team), my_team(), enemies(), num_moved(0) {}
-	void getUnits(DataManager<UnitComponent>& units);
-	int team() { return team_; }
-	UnitComponent* getNext(DataManager<UnitComponent>& units);
+	std::unique_ptr<Command> command;
+	Entity* selected;
+	Player(Level&, int team);
+	inline bool is_done() const { return done_; }
+	void deSelect();
+	void switchCommand(Command* new_command);
+	void deleteCommand();
+	void startTurn(UnitManager& units);
+	void unit_wait(UnitComponent* u);
+	void startTurn();
 	void setMoveState(UnitComponent* u, std::optional<unit::move_state> new_state);
-	void undoLastMove() {
-		num_moved--;
-		assert(num_moved > 0);
-		assert(my_team[num_moved]->movestate == unit::move_state::FREE);
-	}
-};
+	int team() { return team_; }
 
+	UnitComponent* getNext();
+
+	virtual ~Player() {}
+};
