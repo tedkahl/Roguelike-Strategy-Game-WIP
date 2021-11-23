2532e54d2bbb8b8e7c64e377aa04e989c9ca094d
Ted Kahl <tedkahl@gmail.com>
2021-11-23 10:26:52 -0800

    ultimately just moving addTargeter from level to Command

--- WFC/Command.cpp
+++ WFC/Command.cpp
@@ -1,29 +1,30 @@
 #include "Command.h"
 #include "Debug.h"
 #include "Attacks.h"
+#include "PathingGrids.h"
 #include "RealTime.h"
 #include "Actions.h"
-Command::Command(Entity* agent, Level& level_) :agent_(agent), level(&level_) {}
+Command::Command(Entity* agent, Level& level) :agent_(agent), level_(&level) {}
 
 
 void AttackMove::showTargeter() {
 	if (!targeter)
 	{
-		targeter = std::make_unique<Targeter>(paths, level);
+		targeter = std::make_unique<Targeter>(paths, level_);
 	}
 }
-void AttackMove::hideTargeter() {
+void Command::hideTargeter() {
 	delete targeter.release(); //release ownership of Targeter and delete underlying pointer
 }
 
-AttackMove::AttackMove(Entity* agent, Level& level_) : Command(agent, level_), paths(pathFind(agent_->uc(), level->state, getAttack(agent->uc()->stats().attack_type))) {
+AttackMove::AttackMove(Entity* agent, Level& level) : Command(agent, level), paths(pathFind(agent_->uc(), level_->state, getAttack(agent->uc()->stats().attack_type))) {
 }
-AttackMove::AttackMove(Entity* agent, Level& level_, pathsGrid&& known_paths) : Command(agent, level_), paths(std::move(known_paths)) {}
+AttackMove::AttackMove(Entity* agent, Level& level, pathsGrid&& known_paths) : Command(agent, level), paths(std::move(known_paths)) {}
 
 
 std::optional<unit::move_state> AttackMove::execute(sf::Vector2i target, sf::Time now, Player* p_state) {
 	hideTargeter();
-	auto& board = level->state.board;
+	auto& board = level_->state.board;
 	sf::Vector2i move_target = target;
 	bool attack = false;
 	cout << "my loc " << to_string(agent_->getPos()) << endl;
@@ -34,7 +35,7 @@ std::optional<unit::move_state> AttackMove::execute(sf::Vector2i target, sf::Tim
 
 		//if move target taken, choose a new one automatically
 		if (board.at(move_target).unit() && board.at(move_target).unit() != agent_) {
-			target = chooseNewTarget(getAttack(agent_->uc()->stats().attack_type), paths, target, level->state);
+			target = chooseNewTarget(getAttack(agent_->uc()->stats().attack_type), paths, target, level_->state);
 			if (target.x == -1)
 				return std::nullopt;
 		}
@@ -47,28 +48,44 @@ std::optional<unit::move_state> AttackMove::execute(sf::Vector2i target, sf::Tim
 	unit::move_state ret;
 	if (move_path)
 	{
-		agent_->addRT(std::make_unique<GridMove>(move_path.value(), static_cast<object_type>(agent_->type()), sf::seconds(.2f), level->state));
+		agent_->addRT(std::make_unique<GridMove>(move_path.value(), static_cast<object_type>(agent_->type()), sf::seconds(.2f), level_->state));
 		ret = unit::move_state::HAS_MOVED;
 		p_state->setMoveState(agent_->uc(), ret);
 	}
 	if (attack) {
 		//maintain reference to level past object lifespan
-		auto lvlRef = std::make_shared<Level*>(level);
-		entity_action a = [=](Entity* e) {actions::attack(**lvlRef, target, e, e->uc()->stats().damage);};
+		auto lvlRef = std::make_shared<Level*>(level_);
+		auto enemy = board.at(target).unit_uc();
+		entity_action a = [=](Entity* e) {actions::attack(**lvlRef, enemy, e, e->uc()->stats().damage);};
 
-		agent_->addRT(std::make_unique<Lunge>(move_target, target, static_cast<object_type>(agent_->type()), anim_state::ATTACKING, level->state, a));
+		agent_->addRT(std::make_unique<Lunge>(move_target, target, static_cast<object_type>(agent_->type()), anim_state::ATTACKING, level_->state, std::move(a)));
 		ret = unit::move_state::HAS_ACTED;
 		p_state->setMoveState(agent_->uc(), ret);
 	}
 	return static_cast<unit::move_state>(ret);
 }
 
-Targeter::Targeter(pathsGrid& paths, Level* level_) : batch(-1), level(level_) {
-	batch = level->addTargeter(paths);
+template<typename T>
+Targeter::Targeter(const T& targets, Level* level) :batch_(-1), level_(level)
+{
+	batch_ = level_->getDCManager().addBatch();
+	DrawComponent* obj_dc;
+	for (unsigned y = 0;y < targets.grid.height();y++) {
+		for (unsigned x = 0;x < targets.grid.width();x++) {
+			if (targets.grid.at(x, y).search == targets.search) {
+				auto type = getTarget(targets, { static_cast<int>(x), static_cast<int>(y) });
+				/*if (type == target_t::ATTACK_TGT) {
+					assert(paths.grid.at(x, y).attackable);
+				}*/
+				obj_dc = getObjDC(level_->getDCManager(), level_->getTM(), type, batch_);
+				obj_dc->updateEntityPos(targets.offset + sf::Vector2i{ static_cast<int>(x), static_cast<int>(y) }, level_->state.heightmap);
+			}
+		}
+	}
 }
 
 Targeter::~Targeter() {
-	level->removeBatch(batch);
+	level_->removeBatch(batch_);
 }
 
 pathsGrid& AttackMove::getPaths() {
--- WFC/Command.h
+++ WFC/Command.h
@@ -3,28 +3,31 @@
 #include "Team.h"
 #include "Player.h"
 #include "Paths.h"
+#include "PathingGrids.h"
 #include "Level.h"
 #include "GridMove.h"
 class Player;
+
 class Targeter {
 private:
-	Level* level;
-	unsigned batch;
+	Level* level_;
+	int batch_;
 public:
-	Targeter(pathsGrid& paths, Level* level);
+	template<typename T>
+	Targeter(const T& targets, Level* level);
 	~Targeter();
 };
 
 class Command {
 protected:
-	Level* level;
+	Level* level_;
 
 	Entity* agent_;
 	std::unique_ptr<Targeter> targeter;
 public:
-	Command(Entity* agent, Level& level_);
+	Command(Entity* agent, Level&);
 	virtual void showTargeter() = 0;
-	virtual void hideTargeter() = 0;
+	virtual void hideTargeter();
 	virtual std::optional<unit::move_state> execute(sf::Vector2i, sf::Time, Player*) = 0; //target can be changed to its own class if necessary
 	virtual pathsGrid& getPaths() = 0;
 	virtual ~Command() {}
@@ -37,16 +40,16 @@ public:
 	AttackMove(Entity* agent, Level&);
 	AttackMove(Entity* agent, Level&, pathsGrid&& known_paths);
 	virtual void showTargeter() override;
-	virtual void hideTargeter() override;
 	virtual std::optional<unit::move_state> execute(sf::Vector2i target, sf::Time now, Player* p_state) override;
 	virtual pathsGrid& getPaths();
 };
+
 
 //class GenericAnim :public Command {
 //private:
 //	pathsGrid paths;
 //public:
-//	GenericAnim(Entity* agent, Level& level_);
+//	GenericAnim(Entity* agent, Level& level_, RangeFxn, action);
 //	virtual void showTargeter() override;
 //	virtual void hideTargeter() override;
 //	virtual std::optional<unit::move_state> execute(sf::Vector2i target, sf::Time now, Player* p_state) override;
